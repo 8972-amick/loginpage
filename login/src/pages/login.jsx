e3729b7911e24bf59b4bd0ccea95ad3d6efdc564
8972-amick <khadkaamick2@gmail.com>
2025-07-18 12:45:55 -0700

    simple change

--- login/src/pages/login.jsx
+++ login/src/pages/login.jsx
@@ -56,38 +56,38 @@ export default function Loginform(){
     
     return(
         <> 
-        <div class="flex items-center justify-center min-h-screen bg-amber-950 px-4">
-      <form class="bg-amber-800 p-8 rounded-xl shadow-md w-full max-w-sm"
+        <div class="flex items-center justify-center min-h-screen bg-amber-100 px-4">
+      <form class="bg bg-blue-200 p-8 rounded-xl shadow-md w-full max-w-sm"
        onSubmit={handleSubmit}
        >
         <div class="flex justify-center mb-4 text-black-600 text-4xl">
-          <span>👤</span>
+          <span>📚</span>
         </div>
         <h2 class="text-2xl font-bold mb-6 text-center text-black-700">
-          Library Management System Login
+          Library Management System 
         </h2>
 
-        <label class="block text-gray-600 text-sm mb-1">Email</label>
+        <label class="block text-black text-sm mb-1">Email</label>
         <input
           type="email"
-          placeholder="admin@example.com"
+          placeholder="Enter your email"
           Value = {email}
           onChange={(e) => setEmail( e.target.value)}
-          class="w-full px-3 py-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
+          class="w-full px-3 py-2 mb-4 border border-amber-500 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
         />
 
-        <label class="block text-gray-600 text-sm mb-1">Password</label>
+        <label class="block text-black text-sm mb-1">Password</label>
         <input
           type="password"
           placeholder="••••••••"
           Value = {password}
           onChange={(e) => setPassword( e.target.value)}
-          class="w-full px-3 py-2 mb-6 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
+          class="w-full px-3 py-2 mb-6 border  border-amber-500 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
         />
 
         <button
           type="submit"
-          class="w-full bg-black text-white py-2 rounded-md hover:bg-green-700 hover:text-black transition hover:cursor-pointer text-xl"
+          class="w-full bg-black text-white py-2 rounded-md hover:bg-amber-700 hover:text-black transition hover:cursor-pointer text-xl"
         >
             Login
         </button>
